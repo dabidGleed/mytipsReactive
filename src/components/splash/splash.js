f3d08be6cb053185e0b6cf096eeb5ca2339eca03
dabidGleed <dabid@gleedtech.com>
2017-07-04 07:54:25 +0530

    Added new pages to code

---
+++ src/components/splash/splash.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import { StyleSheet, TextInput, View, Image, Button, Alert, TouchableOpacity, Text } from 'react-native';
+import {Actions} from 'react-native-router-flux';
+
+
+export default class Splash extends React.Component {
+  render() {
+     
+    return (  
+      <View style={styles.container}>
+        <View style={styles.imag}>
+          <Image source={require('./../../images/logo1.png')}  style={styles.logo}/>  
+        </View> 
+        <View>
+          <Image source={require('./../../images/background1.png')}  style={styles.backgroundImage}/>  
+        </View> 
+
+     
+        <TouchableOpacity  style={styles.buttonContainer} onPress = {()=> Actions.newPage({})}>
+          <Text  style={styles.buttonText}>CONTINUE</Text>
+          </TouchableOpacity>
+        </View>
+ 
+    );
+  } 
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center', 
+    backgroundColor: '#fff',
+  },
+  ButtonVal:{   
+    position: 'absolute',
+    bottom: 3,
+    padding: 20,
+   
+  },
+  Button:{
+    backgroundColor:'#f0f',
+  },
+   backgroundImage: {
+    resizeMode: 'contain',
+    flex:1,
+    zIndex:0,
+    position:'relative'
+  },
+  logo:{
+      resizeMode: 'contain',
+      width:130,
+      zIndex:1,
+
+  },
+  imag:{
+     position:'absolute',
+      justifyContent: 'center', 
+  },
+  buttonContainer:{
+    backgroundColor: '#1e3808',
+    flex:1,
+    paddingVertical:15,
+    position: 'absolute',
+    bottom: 15,
+    width:'95%', 
+    marginBottom: 120,
+  },
+  buttonText:{
+    textAlign:'center',
+    color:'#fff',
+    fontSize:12,
+    fontWeight: 'bold',
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
